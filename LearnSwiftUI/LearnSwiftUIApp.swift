cc633685f9cb3ede7315ca02cfeb8d093c87c989
Ty. Trương Văn <tytv@vng.com.vn>
2025-07-12 19:13:00 +0700

    Initial Commit

---
+++ LearnSwiftUI/LearnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftUIApp.swift
+//  LearnSwiftUI
+//
+//  Created by MEGABEE on 12/7/25.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
